3966adca1e7357da984347cb5a64b668cb4dfd40
maykolaracayo <maykolaracayo22@gmail.com>
2024-04-29 09:34:11 -0500

    resilencia

--- Mspedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
+++ Mspedido/src/main/java/com/example/mspedido/feign/ClienteFeign.java
@@ -1,6 +1,8 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ClienteDto;
+import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "mscliente-service", path = "/clientes")
 public interface ClienteFeign {
     @GetMapping("/{id}")
-    public ResponseEntity<ClienteDto> insertPorld(@PathVariable(required = true) Integer id);
+    @CircuitBreaker(name = "clientesListarPorIdCB", fallbackMethod = "fallBackCliente")
+    public ResponseEntity<ClienteDto> listById(@PathVariable(required = true) Integer id);
+    default ResponseEntity<ClienteDto> fallBackCliente(Integer id, Exception e) {
+
+        return ResponseEntity.ok(new ClienteDto());
+    }
 }
--- Mspedido/src/main/java/com/example/mspedido/feign/ProductoFeign.java
+++ Mspedido/src/main/java/com/example/mspedido/feign/ProductoFeign.java
@@ -1,6 +1,7 @@
 package com.example.mspedido.feign;
 
 import com.example.mspedido.dto.ProductoDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductoFeign {
     @GetMapping("/{id}")
-    public ResponseEntity<ProductoDto> BuscarPorId(@PathVariable(required = true) Integer id);
+
+    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackProducto")
+    public ResponseEntity<ProductoDto> listById(@PathVariable(required = true) Integer id);
+
+    default ResponseEntity<ProductoDto> fallBackProducto(Integer id, Exception e) {
+        ProductoDto productoDto = new ProductoDto();
+        productoDto.setId(400000);
+        return ResponseEntity.ok(productoDto);
+    }
+
+
 }
 
 
